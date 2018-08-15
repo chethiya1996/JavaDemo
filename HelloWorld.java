77f6f7c277336eb3228f104c173c7b2a5829d3f1
chethiya1996 <chethiya.gunawardana1996@gmail.com>
2018-08-15 16:29:07 +0530

    Error Fixed

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World...");
 		System.out.println("Testing....");
-		system.out.println("Error");
+		System.out.println("Error");
 	}
 }
 
